196e82d6eb10bc0432120b57a992089b1a4c76e7
Krayz k9s <krayzk9s@gmail.com>
2013-04-17 13:16:25 +0000

    Adding Team.cs

---
+++ RickyRaccoon/Team.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace RickyRaccoon
+{
+    [DataContract()]
+    public class Team
+    {
+        public Team(string name, string wincon, bool hidden)
+        {
+            Name = name;
+            WinCon = wincon;
+            hidden = Hidden;
+        }
+        [DataMember]
+        public string Name
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public string WinCon
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public bool Hidden
+        {
+            get;
+            set;
+        }
+        [DataMember]
+        public Team Self
+        {
+            get
+            {
+                return this;
+            }
+        }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+    }
+}
